a69aa1d02c028405bcca02d6f7e267c0eaa92d77
apuntalorapido@gmail.com <apuntalorapido@gmail.com>
2013-09-15 18:35:38 -0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FinalMobil::Application.config.secret_key_base = '5252c84044a4e6605ed9bc930a221964f24617d770fd43650ff9644600bb326c0b08915ef248d092ced40aaf57e8e6d3d03c9257c9d364162d35384c50c3af55'
